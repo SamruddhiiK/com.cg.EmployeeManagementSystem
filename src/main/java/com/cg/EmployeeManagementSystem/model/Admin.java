a8a044c995fbd36adcad3ee499f622a0c3c76158
SamruddhiiK <samruddhi.ashok-kadam@capgemini.com>
2021-09-17 14:48:35 +0530

    c

--- src/main/java/com/cg/EmployeeManagementSystem/model/Admin.java
+++ src/main/java/com/cg/EmployeeManagementSystem/model/Admin.java
@@ -1,7 +1,24 @@
 package com.cg.EmployeeManagementSystem.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="admin_details")
 public class Admin {
+	
+	 @Id
+	 @Column(name="admin_id")
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
       private int adminId;
+	 
+	 @Column(name="admin_name")
       private String adminName;
+	 
+	 @Column(name="admin_name")
       private String adminPassword;
 }
--- src/main/java/com/cg/EmployeeManagementSystem/model/Employee.java
+++ src/main/java/com/cg/EmployeeManagementSystem/model/Employee.java
@@ -1,14 +1,40 @@
 package com.cg.EmployeeManagementSystem.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee_details")
 public class Employee {
-     
+  
+	@Id
+	@Column(name="employee_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int employeeId;
+	
+	@Column(name="employee_name")
 	private String employeeName;
+	
+	@Column(name="employee_password")
 	private String employeePassword;
+	
+	@Column(name="employee_emailId")
 	private String employeeEmailId;
+	
+	@Column(name="employee_contactNo")
 	private int employeeContactNo;
+	
+	@Column(name="employee_doj")
 	private String doj;
+	
+	@Column(name="bankName")
 	private String bankName;
+	
+	@Column(name="bankAccountNo")
 	private String bankAccountNo;
 	//Payroll payroll;
 }
