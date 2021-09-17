e3142600032209cf46fa14d19352218a9652d09e
SamruddhiiK <samruddhi.ashok-kadam@capgemini.com>
2021-09-17 13:45:06 +0530

    Initial commit

---
+++ src/main/java/com/cg/EmployeeManagementSystem/model/Admin.java
@@ -0,0 +1,7 @@
+package com.cg.EmployeeManagementSystem.model;
+
+public class Admin {
+      private int adminId;
+      private String adminName;
+      private String adminPassword;
+}
---
+++ src/main/java/com/cg/EmployeeManagementSystem/model/Employee.java
@@ -0,0 +1,14 @@
+package com.cg.EmployeeManagementSystem.model;
+
+public class Employee {
+     
+	private int employeeId;
+	private String employeeName;
+	private String employeePassword;
+	private String employeeEmailId;
+	private int employeeContactNo;
+	private String doj;
+	private String bankName;
+	private String bankAccountNo;
+	//Payroll payroll;
+}
